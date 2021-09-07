90af5de6cfda12a0c34590198d8786d95f39e048
Marcos Perez <marspzb@gmail.com>
2021-09-06 23:18:00 -0300

    implementacion inicial

---
+++ src/ArbolBinario.java
@@ -0,0 +1,9 @@
+public
+class ArbolBinario
+{
+    private NodoAB raiz;
+
+    public void agregarDato(){
+        raiz=new NodoAB();
+    }
+}
---
+++ src/NodoAB.java
@@ -0,0 +1,6 @@
+public
+class NodoAB
+{
+    private int dato;
+
+}
